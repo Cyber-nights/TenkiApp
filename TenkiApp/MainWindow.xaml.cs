2c777617cd524419c2b240152dbffeb519d2747d
Irbistomin Dmitry <irbistomin@gmail.com>
2021-05-31 20:30:21 +0300

    Entity Framework отлаженный

--- TenkiApp/MainWindow.xaml.cs
+++ TenkiApp/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace TenkiApp
     {
         //App.PogodaContext db = new App.PogodaContext();
         static string save = "saveinfo.txt", savedata = "", link = ""; string[] c = new string[70]; int i = 0;
+        int idcountry, idarea, idcity;
         //FileInfo f = new FileInfo(save);
         StreamReader str = new StreamReader(save);
         public MainWindow()
@@ -32,52 +33,20 @@ namespace TenkiApp
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            /*try
-            {
-                cb1.Items.Clear();
-                using (var db = new App.PogodaContext())
-                {
-                    /*
-                     * var persons = from per in loginconnect.persons
-                                  join co in loginconnect.countrys on per.id_country equals co.id
-                                  orderby per.id
-                                  join pr in loginconnect.professions on per.id_prof equals pr.id
-                                  join ma in loginconnect.machines on per.id_m equals ma.id into mash
-                                  from submash in mash.DefaultIfEmpty()
-                                  select new
-                                  {
-                                      id = per.id,
-                                      name = per.name,
-                                      familia = per.familia,
-                                      otchestvo = per.otchestvo,
-                                      bday = per.bday,
-                                      gender = per.gender,
-                                      inn = per.inn,
-                                      id_country = co.country,
-                                      telefon = per.telefon,
-                                      id_prof = pr.name_prof,
-                                      id_m = submash.machine,
-                                      date_in = per.date_in,
-                                      salary = per.salary
-                                  };
-                    
-                    var c1 = db.countrys.ToList();
-                    foreach (var countr in c1)
-                    { cb1.Items.Add(countr.name); }
-
-                }
-            }
-            catch (Exception exp) { MessageBox.Show(exp.Message + " | Ошибка на этапе запуска, пожалуйста, передайте это разработчику", "Окошко надежды"); }
-*/
             try
             {
+                cb1.Items.Clear();
+                cb1.Items.Clear();
                 using (var db = new App.PogodaContext())
                 {
                     var c1 = from element in db.countrys select element.name;
                     foreach (var countr in c1)
                     { cb1.Items.Add(countr); }
                 }
-
+            }
+            catch (Exception exp) { MessageBox.Show(exp.Message + " | Ошибка на этапе запуска, пожалуйста, передайте это разработчику", "Окошко надежды"); }
+            try
+            {
                 while (!str.EndOfStream)
                 {
                     try
@@ -107,9 +76,6 @@ namespace TenkiApp
                     }
                     Connect.Close();
                 }
-
-
-
                  }
                  catch (Exception exp) { MessageBox.Show(exp.Message + " | Ошибка на этапе запуска, пожалуйста, передайте это разработчику", "Окошко надежды"); }
             */
@@ -132,22 +98,25 @@ namespace TenkiApp
                 cb3.Visibility = Visibility.Hidden;
                 using (var db = new App.PogodaContext())
                 {
-                    var area = from element in db.areas
-                               where element.id_country == (cb1.SelectedIndex + 1)
-                               select element.name;
-                    foreach (var countr in area)
-                    { cb2.Items.Add(countr); }
+                    var area = from element in db.areas where element.id_country == (cb1.SelectedIndex + 1)select element.name;
+                    foreach (var ar in area)
+                    { cb2.Items.Add(ar); }
+                    //var idc = db.countrys.Where(x => x.name == cb1.Text);
                 }
-
             }
             catch (Exception) { MessageBox.Show("Ошибка на первом этапе, пожалуйста, передайте это разработчику", "Окошко надежды"); }
-            /*try
+            
+
+            //
+            
+            try
             {
-                cb2.Items.Clear(); cb3.Items.Clear();
+                /*cb2.Items.Clear(); cb3.Items.Clear();
                 cb2.Visibility = Visibility.Visible;
-                cb3.Visibility = Visibility.Hidden;
+                cb3.Visibility = Visibility.Hidden;*/
                 //
-                using (SQLiteConnection Connect = new SQLiteConnection("Data Source = MyProject.sqlite;"))
+
+                /*using (SQLiteConnection Connect = new SQLiteConnection("Data Source = MyProject.sqlite;"))
                 {
                     Connect.Open();
                     SQLiteCommand command = new SQLiteCommand(@"SELECT name FROM areas WHERE id_country ='" + (cb1.SelectedIndex + 1).ToString() + "';", Connect);
@@ -158,27 +127,34 @@ namespace TenkiApp
                         cb2.Items.Add(d);
                     }
                     Connect.Close();
-                }
+                }*/ 
             }
-            catch (Exception) { MessageBox.Show("Ошибка на первом этапе, пожалуйста, передайте это разработчику", "Окошко надежды"); }
-*/
+            catch (Exception) { MessageBox.Show("Ошибка на первом этапе, пожалуйста, передайте это разработчику", "Окошко надежды");}
         }
         private void cb2_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            /*try
+            try
             {
                 cb3.Items.Clear();
-                using(App.PogodaContext ddb = new App.PogodaContext())
+                using(var db = new App.PogodaContext())
                 {
-                    //var arealink =  
+                    var area = db.areas.Where(x=> x.name == cb2.SelectedItem && x.id_country==cb1.SelectedIndex+1);
+                    foreach (var ci in area)
+                    {
+                        link = ci.link;
+                        idarea = ci.id;
+                    }
+                    var city = db.citys.Where(x => x.id_area == idarea);
+                    if(city.Count() != 0)
+                    foreach(var cit in city)
+                    { cb3.Items.Add(cit.name); cb3.Visibility = Visibility.Visible; } else Info();
                 }
-                if (cb3.Items.Count != 0) cb3.Visibility = Visibility.Visible;
-                Info();
             }
-            catch (Exception) { MessageBox.Show("Ошибка на втором этапе, пожалуйста, передайте это разработчику", "Окошко надежды"); }
-*/
+            catch (Exception у) { MessageBox.Show(у.Message+"Ошибка на втором этапе, пожалуйста, передайте это разработчику", "Окошко надежды"); }
+            
 
-        
+            //
+
             /*try
             {
                 cb3.Items.Clear();
@@ -186,11 +162,11 @@ namespace TenkiApp
                 {
                     Connect.Open();
                     //SELECT arealink FROM Pogodas WHERE countryid = '154' AND areaname <> '';
-                    SQLiteCommand command = new SQLiteCommand(@"SELECT cityname FROM Pogodas WHERE countryid ='" + (cb1.SelectedIndex + 1).ToString() + "' AND areaid = '" + (cb2.SelectedIndex + 1).ToString() + "' AND cityname <> ''", Connect);
-                    SQLiteCommand commandlink = new SQLiteCommand(@"SELECT arealink FROM Pogodas WHERE countryid ='" + (cb1.SelectedIndex + 1).ToString() + "' AND areaid = '" + (cb2.SelectedIndex + 1).ToString() + "' AND arealink <> ''", Connect);
-                    SQLiteDataReader dannlink = commandlink.ExecuteReader();
-                    SQLiteDataReader dann = command.ExecuteReader();
-                    while (dann.Read())
+                    /*SQLiteCommand command = new SQLiteCommand(@"SELECT name FROM areas WHERE id_country ='" + (cb1.SelectedIndex + 1).ToString() + "' AND id = '" + (cb2.SelectedIndex + 1).ToString() + ";", Connect);
+                    SQLiteCommand commandlink = new SQLiteCommand(@"SELECT link FROM areas WHERE id_country ='" + (cb1.SelectedIndex + 1).ToString() + "' AND id = '" + (cb2.SelectedIndex + 1).ToString() + ";", Connect);
+                   SQLiteDataReader dannlink = commandlink.ExecuteReader();
+                    SQLiteDataReader dann = command.ExecuteReader();*/ 
+                    /*while (dann.Read())
                     {
                         object d = dann[0];
                         cb3.Items.Add(d);
@@ -205,10 +181,26 @@ namespace TenkiApp
                 if (cb3.Items.Count != 0) cb3.Visibility = Visibility.Visible;
                 Info();
             }
-            catch (Exception) { MessageBox.Show("Ошибка на втором этапе, пожалуйста, передайте это разработчику", "Окошко надежды"); } */       }
+            catch (Exception) { MessageBox.Show("Ошибка на втором этапе, пожалуйста, передайте это разработчику", "Окошко надежды"); }*/
+        }
         private void cb3_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             try
+            {
+                using (var db = new App.PogodaContext())
+                {
+                    var city = db.citys.Where(x => x.name == cb3.SelectedItem);
+                    foreach(var cit in city)
+                    {link = cit.link;}
+                    if (city.Count() != 0) Info();
+                }
+            }
+            catch (Exception) { MessageBox.Show("Ошибка на третьем этапе, пожалуйста, передайте это разработчику", "Окошко надежды"); }
+            
+
+            //
+            
+            /*try
             {
                 using (SQLiteConnection Connect = new SQLiteConnection("Data Source = MyProject.sqlite;"))
                 {
@@ -225,6 +217,7 @@ namespace TenkiApp
                 Info();
             }
             catch (Exception) { MessageBox.Show("Ошибка на третьем этапе, пожалуйста, передайте это разработчику", "Окошко надежды"); }
+        */
         }
         private void b3_Click(object sender, RoutedEventArgs e)//проверка данных
         {
@@ -330,31 +323,37 @@ namespace TenkiApp
 
         public void Info()
         {
+            i = 0;
             try
             {
-                i = 0;
+                HtmlWeb webinfo = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument docwebinfo = webinfo.Load(link);
+                HtmlNode[] temperature;
+                temperature = docwebinfo.DocumentNode.SelectNodes("//div [@class='fact__temp-wrap'] //span[@class='temp__value temp__value_with-unit']").ToArray();
+                foreach (HtmlNode t in temperature)
+                { l1.Content = "Текущая температура " + t.InnerText; i = 0; break; }
                 try
                 {
-                    HtmlWeb webinfo = new HtmlWeb();
-                    HtmlAgilityPack.HtmlDocument docwebinfo = webinfo.Load(link);
-                    HtmlNode[] temperature;
-                    temperature = docwebinfo.DocumentNode.SelectNodes("//div [@class='fact__temp-wrap'] //span[@class='temp__value temp__value_with-unit']").ToArray();
-                    foreach (HtmlNode t in temperature)
-                    { l1.Content = "Текущая температура " + t.InnerText; i = 0; break; }
                     HtmlNode[] wind = docwebinfo.DocumentNode.SelectNodes("//div[@class='term__value'] //span [@class='wind-speed']").ToArray();
                     foreach (HtmlNode w in wind)
                     { l2.Content = "Скорость ветра " + w.InnerText + " м/с"; break; }
-                    HtmlNode[] pressure = docwebinfo.DocumentNode.SelectNodes("//div [@class='term__value']").ToArray();
-                    foreach (HtmlNode p in pressure)
-                    { i++; if (i == 5) { l3.Content = p.InnerText; i = 0; break; } }
-                    HtmlNode[] humidity = docwebinfo.DocumentNode.SelectNodes("//div [@class='term__value']").ToArray();
-                    foreach (HtmlNode h in humidity)
-                    { i++; if (i == 4) { l4.Content = "Влажность " + h.InnerText; i = 0; break; } }
-                    // температура днем //div[@class='temp forecast-briefly__temp forecast-briefly__temp_day']
                 }
-                catch { l1.Content = "Ошибка"; l2.Content = "Ошибка"; }
+                catch (ArgumentNullException) { l2.Content = "Штиль"; };
+                HtmlNode[] pressure = docwebinfo.DocumentNode.SelectNodes("//div [@class='term__value']").ToArray();
+                foreach (HtmlNode p in pressure)
+                { i++; if (i == 5) { l3.Content = p.InnerText; i = 0; break; } }
+                HtmlNode[] humidity = docwebinfo.DocumentNode.SelectNodes("//div [@class='term__value']").ToArray();
+                foreach (HtmlNode h in humidity)
+                { i++; if (i == 4) { l4.Content = "Влажность " + h.InnerText; i = 0; break; } }
+                // температура днем //div[@class='temp forecast-briefly__temp forecast-briefly__temp_day']
             }
-            catch (Exception) { MessageBox.Show("Данные не получены", "Окошко надежды"); }
+            catch (Exception exp)
+            {
+                l1.Content = "Данные не получены";
+                l2.Content = "Данные не получены";
+                l3.Content = "Данные не получены";
+                l4.Content = "Данные не получены";
+            }
         }
         public async void adddatabase()
         {
@@ -407,71 +406,3 @@ namespace TenkiApp
         }
     }
 }
-
-/*
- HtmlWeb webcountry = new HtmlWeb();
-
-HtmlAgilityPack.HtmlDocument doccountry = webcountry.Load(c);
-
-HtmlNode[] nodescountry = doccountry.DocumentNode.SelectNodes(tegcountryarea).ToArray();
-
-foreach (HtmlNode itemcountry in nodescountry)
-
-{
-
-await db.BDPogodas.AddRangeAsync(new BDPogoda { countryid = (i + 1).ToString(), countryname = itemcountry.InnerText, countrylink = "https://yandex.ru" + itemcountry.GetAttributeValue("href", "") });
-
-await db.SaveChangesAsync();
-
-HtmlAgilityPack.HtmlDocument docarea = new HtmlWeb().Load("https://yandex.ru" + itemcountry.GetAttributeValue("href", ""));//получение ссылки
-
-HtmlNode[] nodesarea = docarea.DocumentNode.SelectNodes(tegcountryarea).ToArray();
-
-foreach (HtmlNode item in nodesarea)
-
-{
-
-await db.BDPogodas.AddRangeAsync(new BDPogoda { countryid = (i + 1).ToString(), areaid = (j + 1).ToString(), areaname = item.InnerText, arealink = "https://yandex.ru" + item.GetAttributeValue("href", "") });
-
-await db.SaveChangesAsync();
-
-HtmlNode[] nodescity = null;
-
-if (citys == true)
-
-{
-
-try
-
-{
-
-HtmlAgilityPack.HtmlDocument doccity = new HtmlWeb().Load("https://yandex.ru" + item.GetAttributeValue("href", ""));
-
-nodescity = doccity.DocumentNode.SelectNodes(tegcountryarea).ToArray();
-
-foreach (HtmlNode city in nodescity)
-
-{
-
-await db.BDPogodas.AddRangeAsync(new BDPogoda { countryid = (i + 1).ToString(), areaid = (j + 1).ToString(), cityid = (g + 1).ToString(), cityname = city.InnerText, citylink = "https://yandex.ru" + city.GetAttributeValue("href", "") });
-
-await db.SaveChangesAsync(); g++;
-
-}
-
-}
-
-catch (ArgumentNullException) { }
-
-}
-
-g = 0; j++; if (nodescity == null) citys = false;
-
-}
-
-j = 0; i++; citys = true;
-
-}
-
-}
-*/
